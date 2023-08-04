ea9c797160e735d64cc1044fac26ff720a4fb903
honeydanji <129818881+honeydanji@users.noreply.github.com>
2023-08-04 09:41:35 +0900

    동물병원 웹 서비스

---
+++ pethospital/src/main/java/com/pethospital/domain/Pet_honey_reply.java
@@ -0,0 +1,41 @@
+package com.pethospital.domain;
+
+import java.security.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Pet_honey_reply {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="comment_id")
+    private Long commentId;
+    
+    @Column(name="honey_board_id", nullable = false)
+    private int honeyBoardId;
+
+    @Column(name="user_id", unique = true)
+    private String userId;
+    private String nickname;
+    
+    @Column(nullable = false)
+    private String contents;
+    
+    @Column(name="registration_date")
+    private Timestamp registrationDate;
+}
